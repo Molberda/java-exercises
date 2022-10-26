570db3df1a80795572241af31a48e293fa38dce7
David <davidmollera@hotmail.com>
2022-10-26 13:42:56 -0500

    created a function to add all of the elements in an array

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-let arr = [1, 2, 3 ,4 ,5]
-
-function arrLength(arr) {
-    return arr[arr.length - 1]
+let arr = [1, 2, 6 ,4 ,5]
+let sum = 0
+function arrAddition(arr) {
+    for (let i = 0; i < arr.length; ++i){
+        sum = sum + arr[i]
+    }
+    return sum
 }
 
-console.log(arrLength(arr));
+console.log(arrAddition(arr));
