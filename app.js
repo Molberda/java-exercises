4d40a77033d3cf7e6b0d60b90e12398be02a2619
David <davidmollera@hotmail.com>
2022-10-27 16:28:36 -0500

    created a function to return the inital array but replacing every value with 0, using a for loop

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 function zero(arr) {
-    let newArr = arr.map(elem => elem * 0)
+    let newArr = []
+    for (let i = 0; i < arr.length; i++){
+        newArr [i] = 0
+    }
     return newArr
 } 
 
 console.log(zero([1, 2, 3, 4, 5, 67,]));
+
+// function zeroMap(zeroArr) {
+//     let zeroArr1 = zeroArr.map(elem => elem * 0)
+//     return zeroArr1
+// }
+
+// console.log(zeroMap([1,2,3,4,5,6]))
