ad57af886b0ed4dd0b3d8f76334ea60a5deb12be
David <davidmollera@hotmail.com>
2022-10-27 13:51:36 -0500

    created a function to rewrite every element of an array into a zero, this was using .map()

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-function reverse(str) {
-    return str.split('').reverse().join('')
-}
+function zero(arr) {
+    let newArr = arr.map(elem => elem * 0)
+    return newArr
+} 
 
-console.log(reverse('amazing'));
+console.log(zero([1, 2, 3, 4, 5, 67,]));
