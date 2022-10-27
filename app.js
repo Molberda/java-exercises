1401be5696c0c873f16c8894839f615aa27380f3
David <davidmollera@hotmail.com>
2022-10-27 17:43:32 -0500

    created a function to filter out the falsey values from a given array, made it using two methods, using a for loop and using .filter

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-function removeApples(food) {
-    return food.filter(elem => elem !== 'apple')
+function filterFalsey(arr) {
+   return arr.filter(elem => !!elem === true)
+}
+console.log(filterFalsey(['', 0, null, [], undefined, '0']));
+
+function filterFalsey(arr){
+    let truthArr = []
+    for (let i = 0; i < arr.length; ++i){
+        if (!!arr[i] === true){
+            truthArr.push(arr[i])
+        }
+    }
+    return truthArr
 }
 
-console.log(removeApples(["banana", "apple", "orange", "apple"]));
+console.log(filterFalsey(['', 0, null, undefined, '0', 12, []]));
