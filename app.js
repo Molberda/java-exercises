bc70a54cbbc331ed53d0fd579b87343c7eda70cd
David <davidmollera@hotmail.com>
2022-10-27 16:37:39 -0500

    created a function that returns the originals array elements but mapped to 0

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-function zero(arr) {
-    return new Array (arr.length).fill(0)
-} 
+function zeroMap(zeroArr) {
+    return zeroArr.map(elem => elem * 0 )
+}
 
-console.log(zero([1, 2, 3, 4, 5, 67,]));
-
-// function zeroMap(zeroArr) {
-//     let zeroArr1 = zeroArr.map(elem => elem * 0)
-//     return zeroArr1
-// }
-
-// console.log(zeroMap([1,2,3,4,5,6]))
+console.log(zeroMap([1,2,3,4,5,6]))
