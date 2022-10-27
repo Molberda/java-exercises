7ea33399d371d567625e9c3bc44a3f75885b9b56
David <davidmollera@hotmail.com>
2022-10-27 17:03:19 -0500

    created a function to filter out 'apple' from a given array, using for loop

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function zeroMap(zeroArr) {
-    return zeroArr.map(elem => elem * 0 )
+function removeApples(food){
+    let noApples = []
+    for (let i = 0; i < food.length; ++i){
+        if (food[i] !== "apple"){
+            noApples.push(food[i])
+        }
+    }
+    return noApples
 }
 
-console.log(zeroMap([1,2,3,4,5,6]))
+console.log(removeApples(["banana", "apple", "orange", "apple"]))
+
+
+// function removeApples(food) {
+//     let noApples = food.filter((elem) => {
+//     if (elem !== "apple") {
+//         return true
+//     }
+//   });
+// }
+
+// console.log(removeApples("banana", "apple", "orange", "apple"));
