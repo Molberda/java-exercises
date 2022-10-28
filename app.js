e09140d90d72b866faf6e91980aeee05f5862276
David <davidmollera@hotmail.com>
2022-10-27 21:53:17 -0500

    created a function to sort from smallest to biggest a given objects and organizing by the price of the object, using sort and the special command to sort numerically and not alphabetically

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-function showRating(arr){
-    return arr.sort((a, b) => a - b)
+function organizeObjects(arr) {
+  return arr.sort((a, b) => a.price - b.price);
 }
-console.log(showRating([20, 50, 40, 70, 10]))
+console.log(
+  organizeObjects([
+    { id: 3, price: 60 },
+    { id: 1, price: 50 },
+    { id: 2, price: 30 },
+  ])
+);
